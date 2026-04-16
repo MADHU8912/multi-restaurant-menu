2bb37c2fc90afd50a70a2140eee152ff3a92ff63
MADHU8912 <nikhilabba12@gmail.com>
2026-04-16 13:28:33 +0530

    Added multiple restaurants

--- frontend/app.js
+++ frontend/app.js
@@ -1,20 +1,29 @@
 fetch("https://multi-restaurant-menu.onrender.com/restaurants")
   .then(response => response.json())
   .then(data => {
-    console.log(data);
-
     const container = document.getElementById("restaurants");
 
     data.forEach(restaurant => {
       const div = document.createElement("div");
+      div.className = "card";
+
       div.innerHTML = `
         <h2>${restaurant.name}</h2>
         <p>${restaurant.type}</p>
         <p>${restaurant.location}</p>
+        <button onclick="viewMenu(${restaurant.id})">View Menu</button>
       `;
+
       container.appendChild(div);
     });
+
+    window.allRestaurants = data;
   })
   .catch(error => {
     console.error("Error:", error);
   });
+
+function viewMenu(id) {
+  localStorage.setItem("selectedRestaurantId", id);
+  window.location.href = "menu.html";
+}
